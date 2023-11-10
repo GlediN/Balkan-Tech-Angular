22e4d4a14df990de9e9176b0f7f0e11655ead06e
Gledi <gledinezha1@gmail.com>
2023-11-10 17:40:41 +0100

    Added order history downoload button working on user details

--- src/app/admin/purchase-history/purchase-history.component.ts
+++ src/app/admin/purchase-history/purchase-history.component.ts
@@ -3,6 +3,8 @@ import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
 import {faMoneyBill1Wave} from "@fortawesome/free-solid-svg-icons/faMoneyBill1Wave";
 import {faShoppingBasket} from "@fortawesome/free-solid-svg-icons/faShoppingBasket";
 import {faUserCircle} from "@fortawesome/free-regular-svg-icons";
+import {PurchaseHistoryUserService} from "./purchase-history-user.service";
+import {UserService} from "../user-details/user.service";
 
 @Component({
   selector: 'app-purchase-history',
@@ -15,29 +17,48 @@ faMoeny=faMoneyBill1Wave;
 faBasket=faShoppingBasket;
   protected readonly faUserCircle = faUserCircle;
 
-users = [
-    {
-      id:"1",
-      date:"12/12/1222",
-      name:"emri",
-      surname:"mbiemri",
-      email:"asdasd@gmail.com",
-      address:"adresa",
-      receipt:"asdasdasd.pdf",
-  totalPrice:"150"
-    },
-  {
-    id:"2",
-    date:"12/12/1222",
-    name:"emri2",
-    surname:"mbiemri2",
-    email:"asdasd@gmail.com2",
-    address:"adresa2",
-    receipt:"asdasdasd2.pdf",
-    totalPrice:"1502"
-  },
-
+users = [{
+  id:'',
+  date:'',
+  name:'',
+  surname:'',
+  email:'',
+  address:'',
+  receipt:'',
+  totalPrice:''
+  //users i merr nga purchase-history-user.service.ts
+}
     ]
+  constructor(private userService: UserService) { }
+
+  ngOnInit() {
+    // Fetch user data when the component initializes
+    this.userService.getUsers().subscribe(users => {
+      this.users = users;
+      console.log('Fetched users:', this.users);
+    });
+  }
+
+  downloadJson(userId: String) { // Adjust the type to 'number' if IDs are integers
+    // Find the user with the specified ID
+    const user = this.users.find(u => u.id === userId);
+
+    // Ensure the user is found before proceeding
+    if (user) {
+      const jsonString = JSON.stringify(user, null, 2);
+      const blob = new Blob([jsonString], { type: 'application/json' });
+
+      const link = document.createElement('a');
+      link.href = window.URL.createObjectURL(blob);
+      link.download = `user_data_${userId}.json`; // Include user ID in the file name
+
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    } else {
+      console.error(`User with ID ${userId} not found`);
+    }
+  }
 
 
 
--- src/app/admin/user-details/user-details.component.ts
+++ src/app/admin/user-details/user-details.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {booleanAttribute, Component} from '@angular/core';
+import {UserService} from "./user.service";
+import {PurchaseHistoryUserService} from "../purchase-history/purchase-history-user.service";
 
 @Component({
   selector: 'app-settings',
@@ -7,4 +9,43 @@ import { Component } from '@angular/core';
 })
 export class UserDetailsComponent {
 
+  users=[{
+    id:'',
+    date_of_register:'',
+    name:'',
+    surname:'',
+    email:'',
+    address:'',
+    isAdmin:null
+  }]
+  constructor(private userService: PurchaseHistoryUserService) { }
+
+  ngOnInit() {
+    // Fetch user data when the component initializes
+    this.userService.getUsers().subscribe(users => {
+      this.users = users;
+      console.log('Fetched users:', this.users);
+    });
+  }
+
+  // downloadJson(userId: String) { // Adjust the type to 'number' if IDs are integers
+  //   // Find the user with the specified ID
+  //   const user = this.users.find(u => u.id === userId);
+  //
+  //   // Ensure the user is found before proceeding
+  //   if (user) {
+  //     const jsonString = JSON.stringify(user, null, 2);
+  //     const blob = new Blob([jsonString], { type: 'application/json' });
+  //
+  //     const link = document.createElement('a');
+  //     link.href = window.URL.createObjectURL(blob);
+  //     link.download = `user_data_${userId}.json`; // Include user ID in the file name
+  //
+  //     document.body.appendChild(link);
+  //     link.click();
+  //     document.body.removeChild(link);
+  //   } else {
+  //     console.error(`User with ID ${userId} not found`);
+  //   }
+  // }
 }
