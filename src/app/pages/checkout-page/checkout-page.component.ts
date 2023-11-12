27cb7bf449b47e04c1c6a5146bb19f88681ce10e
Gledi <gledinezha1@gmail.com>
2023-11-12 13:33:31 +0100

    Close to finishing checkout

--- src/app/pages/checkout-page/checkout-page.component.ts
+++ src/app/pages/checkout-page/checkout-page.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import {CartService} from "../../services/cart.service";
+import {Component} from '@angular/core';
+import {Router} from "@angular/router";
+import {CheckoutService} from "../../services/checkout.service";
+
 
 @Component({
   selector: 'app-checkout-page',
@@ -7,27 +9,56 @@ import {CartService} from "../../services/cart.service";
   styleUrls: ['./checkout-page.component.scss']
 })
 export class CheckoutPageComponent {
+  constructor(private checkoutService:CheckoutService, private router: Router) {
+  }
+  getTotalPrice(clientOrder: string | any[]) {
+    let totalPrice = 0;
+
+    for (let i = 0; i < clientOrder.length; i++) {
+      // Assuming the 'price' property contains numerical values
+      const productPrice = parseFloat(clientOrder[i].price);
+
+      // Check if the conversion to a number was successful
+      if (!isNaN(productPrice)) {
+        totalPrice += productPrice;
+      } else {
+        console.error(`Invalid price for product '${clientOrder[i].productname}'`);
+      }
+    }
+
+    return totalPrice;
+  }
+
+  name: string = '';
+  surname: string = '';
+  contactNumber: string = '';
+  email: string = '';
+  address: string = '';
+  country: string = '';
+  clientOrder = [
+    {
+      productname: '',
+      price: 0,
+      quantity: 0,
+    },
+    {
+      productname: '',
+      price: 0,
+      quantity: 0,
+    }]
+  totalPrice: number = this.getTotalPrice(this.clientOrder);
+
 
   userData = [
     {
       firstname: '',
       lastname: 'test1',
       email: 'test2',
-      phone:'test3',
-      address:'test4',
-      country:'test5',
-      city:'test7',
+      phone: 'test3',
+      address: 'test4',
+      country: 'test5',
+      city: 'test7',
 
     },]
-  constructor(private cartService: CartService) {
-    console.log('Cart items in CheckoutPageComponent:', this.clientOrder);
-
-  }
-  get clientOrder() {
-    return this.cartService.getCartItems();
-  }
-  getTotal() {
-    return this.clientOrder.reduce((total, item) => total + (parseFloat(item.product.price) * item.quantity), 0);
-  }
 
 }
